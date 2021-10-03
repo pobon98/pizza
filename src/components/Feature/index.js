3663c84e962de492801f26aff4da7db7fe31d3d7
Poban Das <pobondas211@gmail.com>
2021-10-03 16:37:54 +0530

    first commit

---
+++ src/components/Feature/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Feature = () => {
+    return (
+        <FeatureContainer>
+            <h1>Pizza of the Day</h1>
+            <p>Get Great Pizza daily available 24x7</p>
+            <FeatureButton>Order Now</FeatureButton>
+        </FeatureContainer>
+    )
+}
+
+export default Feature;
---
+++ src/components/Products/data.js
@@ -0,0 +1,31 @@
+import product1 from '../../images/product-1.jpg';
+import product2 from '../../images/product-2.jpg';
+import product3 from '../../images/product-3.jpg';
+
+export const productData = [
+    {
+        img: product1,
+        alt: 'Pizza',
+        name: 'Supreme Pizza',
+        desc: 'Marinara sauce, basil, italian sausage, roma tomatotoes, olives, and pesto',
+        prices: '$19.99',
+        button: 'Add to Cart'
+    },
+    {
+        img: product2,
+        alt: 'Pizza',
+        name: 'Supreme Pizza',
+        desc: 'Marinara sauce, basil, italian sausage, roma tomatotoes, olives, and pesto',
+        prices: '$19.99',
+        button: 'Add to Cart'
+    },
+    {
+        img: product3,
+        alt: 'Pizza',
+        name: 'Supreme Pizza',
+        desc: 'Marinara sauce, basil, italian sausage, roma tomatotoes, olives, and pesto',
+        prices: '$19.99',
+        button: 'Add to Cart'
+    }
+        
+];
