eefbbd0a278dd6dde7dec2d75ce1fa90379536c6
Poban Das <pobondas211@gmail.com>
2021-10-03 17:07:12 +0530

    Final commit

--- src/components/Products/ProductsElements.js
+++ src/components/Products/ProductsElements.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const ProductsContainer = styled.div`
-  width: 100vw;
+  width: 100%;
   min-height: 100vh;
   padding: 5rem calc((100vw - 1300px) /2);
   background: #150f0f;
@@ -9,7 +9,7 @@ export const ProductsContainer = styled.div`
 `;
 export const ProductWrapper = styled.div`
   display: flex;
-  flex-wrap: wrap;K
+  flex-wrap: wrap;
   justify-content: center;
   margin: 0 auto;
 `;
